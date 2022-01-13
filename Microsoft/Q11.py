05358b014130d8fb722a11672e43dea8465d9163
chinmayaparakh <54195612+chinmayaparakh@users.noreply.github.com>
2022-01-13 23:47:07 +0530

    Create Q11.py

---
+++ Microsoft/Q11.py
@@ -0,0 +1,38 @@
+
+#Function to generate binary numbers from 1 to N using a queue.
+def generate(N):
+    # code here
+    ans=[]
+    
+    for i in range(1,N+1):
+        ans.append(bin(i).replace("0b",""))
+    return ans
+        
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+#Contributed by :  Nikhil Kumar Singh(nickzuck_007)
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n = int(input())
+        res = generate(n)
+        for i in range (len (res)):
+            print (res[i], end=" ")
+        print()
+# } Driver Code Ends
